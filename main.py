7e20879f0a6730f6eb975d3a9b2571833be57a16
FranMulti1200 <fbautista848stf@gmail.com>
2024-11-11 19:38:35 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/photo.png")
-
 with col2:
     st.title("Francisco Bautista")
     content = """
@@ -15,3 +14,8 @@ with col2:
         incluyendo la programación y uso de los complejos y flexibles PLC´s (Siemens/Omron)
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
