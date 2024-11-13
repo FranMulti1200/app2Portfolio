d68d60d866d8f87a903eebba021c0b6608a86ef3
FranMulti1200 <fbautista848stf@gmail.com>
2024-11-13 11:41:41 +0100

    Day 23: Send email with submit button,

--- send_email.py
+++ send_email.py
@@ -1,20 +1,17 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "franciscoalberto.bautista.fp@iescampanillas.com"
-password = "pjxr mnnt gfmo nmzv"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "franciscoalberto.bautista.fp@iescampanillas.com"
-context = ssl.create_default_context()
+    username = "franciscoalberto.bautista.fp@iescampanillas.com"
+    password = "pjxr mnnt gfmo nmzv"
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
+    receiver = "franciscoalberto.bautista.fp@iescampanillas.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
