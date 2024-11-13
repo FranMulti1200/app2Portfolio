ee3065a0ed1fc0f19d34dc97644ce9564bbbc52e
FranMulti1200 <fbautista848stf@gmail.com>
2024-11-13 13:03:38 +0100

    Day 23: Secret password

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -7,11 +7,11 @@ with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message")
     message = f"""\
-        Subject: New email form {user_email}
+Subject: New email form {user_email}
 
-        From: {user_email}
-        {raw_message}
-        """
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button()
     if button:
         send_email(message)
